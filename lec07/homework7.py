6b4573e864a3ca0614fe43891c3e8e97941aa6c9
7amuMiNT <oaerixono@gmail.com>
2026-02-26 16:07:23 +0800

    Update homework7.py

--- lec07/homework7.py
+++ lec07/homework7.py
@@ -16,8 +16,19 @@ def major_chord(f, Fs):
     (2) A major third, i.e., four semitones above f
     (3) A major fifth, i.e., seven semitones above f
     '''
-    raise RuntimeError("You need to write this part")
+    # half-second duration
+    N = int(np.round(0.5 * Fs))
+    n = np.arange(N)
 
+    # major third: +4 semitones, major fifth: +7 semitones
+    f3 = f * (2 ** (4/12))
+    f5 = f * (2 ** (7/12))
+
+    # sum of three cosines (optionally scaled to avoid clipping)
+    x = (np.cos(2*np.pi*f*n/Fs) +
+         np.cos(2*np.pi*f3*n/Fs) +
+         np.cos(2*np.pi*f5*n/Fs)) / 3.0
+    return x
 def dft_matrix(N):
     '''
     Create a DFT transform matrix, W, of size N.
@@ -29,8 +40,10 @@ def dft_matrix(N):
     W (NxN array): a matrix of dtype='complex' whose (k,n)^th element is:
            W[k,n] = cos(2*np.pi*k*n/N) - j*sin(2*np.pi*k*n/N)
     '''
-    raise RuntimeError("You need to write this part")
-
+    n = np.arange(N)
+    k = np.arange(N)
+    W = np.exp(-1j * 2*np.pi * np.outer(k, n) / N).astype(complex)
+    return W
 def spectral_analysis(x, Fs):
     '''
     Find the three loudest frequencies in x.
@@ -43,4 +56,19 @@ def spectral_analysis(x, Fs):
     f1, f2, f3: The three loudest frequencies (in Hertz)
       These should be sorted so f1 < f2 < f3.
     '''
-    raise RuntimeError("You need to write this part")
+    x = np.asarray(x)
+    N = len(x)
+
+    W = dft_matrix(N)
+    X = W @ x
+    mag = np.abs(X)
+
+    # use only nonnegative frequencies (unique bins for real signals)
+    kmax = N // 2
+    mag_pos = mag[:kmax+1]
+
+    # pick 3 largest bins
+    topk = np.argsort(mag_pos)[-3:]
+    freqs = np.sort(topk * Fs / N)
+
+    return float(freqs[0]), float(freqs[1]), float(freqs[2])
